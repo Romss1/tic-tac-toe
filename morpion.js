f475653143baddd0e609784e5ef0532b7b15d206
Romss1 <romanberth@gmail.com>
2021-07-06 18:35:15 +0200

    test

--- morpion.js
+++ morpion.js
@@ -1,10 +1,23 @@
 let btnStart = document.getElementById('start');
 let btnPause = document.getElementById('pause');
 let btnResume = document.getElementById('resume');
+let seconde = document.getElementById('seconde');
 let time = document.getElementById('time');
 let intervalTime = null;
 theBtn = false;
 const cases = document.querySelectorAll("*[id^='case']");
+
+let win = false;
+const possibiltyList = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7]
+]
 
 function startTimer() {
     if (theBtn == false) {
@@ -20,9 +33,14 @@ function startTimer() {
 }
 
 function updateChrono() {
-
-    let currentValue = parseInt(time.innerText);
-    time.innerText = currentValue + 1;
+    if (seconde.innerText < 59) {
+        let currentValue = "0" + parseInt(seconde.innerText);
+        seconde.innerText = currentValue + 1;
+    } else if(seconde.innerText = 59){
+        let currentValueMinute = parseInt(time.innerText)
+        time.innerText = currentValueMinute +1;
+        seconde.innerText = '0';
+    }
 }
 
 
@@ -31,23 +49,31 @@ function resumeTimer() {
     if (theBtn == false) {
         clearInterval(intervalTime);
         intervalTime = null;
-        time.innerText = 0;
+        seconde.innerText = '00';
+        time.innerText = '00';
     } else if (theBtn == true) {
         clearInterval(intervalTime);
-        time.innerText = 0;
+        seconde.innerText = '00';
+        time.innerText = '00';
         intervalTime = setInterval(updateChrono, 1000);
     }
 }
 
-function display(event){
+function display(event) {
     let target = event.target.querySelector("img");
-   target.setAttribute("src",'circle.png');
-   target.style.display = 'block';
+    target.setAttribute("src", 'circle.png');
+    target.style.display = 'block';
 }
+
 
 btnStart.addEventListener('click', startTimer);
 btnResume.addEventListener('click', resumeTimer);
 
-cases.forEach((item)=>{
+cases.forEach((item) => {
     item.addEventListener('click', display);
 })
+
+for(let possibilty of possibiltyList){
+    console.log(possibilty)
+}
+
