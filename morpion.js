39f245cf04d3f90afb6ecc4ca8c7e2976330e0ac
Romss1 <romanberth@gmail.com>
2021-07-08 15:02:53 +0200

    08/07/21

--- morpion.js
+++ morpion.js
@@ -2,9 +2,12 @@ let btnStart = document.getElementById('start');
 let btnPause = document.getElementById('pause');
 let btnResume = document.getElementById('resume');
 let time = document.getElementById('time');
+let second = document.getElementById('second');
 let intervalTime = null;
 theBtn = false;
+let circle = true;
 const cases = document.querySelectorAll("*[id^='case']");
+
 
 function startTimer() {
     if (theBtn == false) {
@@ -20,9 +23,14 @@ function startTimer() {
 }
 
 function updateChrono() {
-
-    let currentValue = parseInt(time.innerText);
-    time.innerText = currentValue + 1;
+    if (second.innerText < 59) {
+        let currentValue = parseInt(second.innerText);
+        second.innerText = currentValue + 1;
+    } else if (second.innerText = 59){
+        let currentValueMinute = parseInt(time.innerText);
+        second.innerText = '0';
+        time.innerText = currentValueMinute +1;
+    }
 }
 
 
@@ -32,22 +40,33 @@ function resumeTimer() {
         clearInterval(intervalTime);
         intervalTime = null;
         time.innerText = 0;
+        second.innerText =0;
     } else if (theBtn == true) {
         clearInterval(intervalTime);
         time.innerText = 0;
+        second.innerText =0;
         intervalTime = setInterval(updateChrono, 1000);
     }
 }
 
-function display(event){
+function display(event) {
+    
     let target = event.target.querySelector("img");
-   target.setAttribute("src",'circle.png');
-   target.style.display = 'block';
+    if (circle == true) {
+        target.setAttribute("src", 'cross.png');
+        target.style.display = 'block';
+        circle = false;
+        console.log(circle);
+    } else if (circle == false) {
+        target.setAttribute("src", 'circle.png');
+        target.style.display = 'block';
+        circle = true;
+    }
 }
 
 btnStart.addEventListener('click', startTimer);
 btnResume.addEventListener('click', resumeTimer);
 
-cases.forEach((item)=>{
+cases.forEach((item) => {
     item.addEventListener('click', display);
 })
